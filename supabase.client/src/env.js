effe7cbd7f9d8e7797c1185bd57c67a88be417d9
JordanLDiaz <jordanldiaz29@gmail.com>
2023-08-03 17:01:00 -0600

    practice

--- supabase.client/src/env.js
+++ supabase.client/src/env.js
@@ -1,9 +1,9 @@
+
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'mickshanny.us.auth0.com'
-export const clientId = 'luwNBP378EfKtXGGgxJj9T2kNZ1A562I'
-export const audience = 'http://Sandbox.com'
-export const supabaseUrl = 'https://uoxkuoksvnbowxkngjfv.supabase.co'
-export const supabasePublicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVveGt1b2tzdm5ib3d4a25namZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM5OTI0NzUsImV4cCI6MTk4OTU2ODQ3NX0.eASvd_GyArE73HAMywcIPDUD7CAJsfUqlWE0RCNaZo4'
-
+export const domain = 'dev-wra2r4u13787lot5.us.auth0.com'
+export const clientId = 'Wcq0CtKAqonSFxcd1VwYcURAxkuwcs8t'
+export const audience = 'https://classroom.com'
+export const supabaseUrl = 'https://lantcqhludlhmpsxhzzj.supabase.co'
+export const supabasePublicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhbnRjcWhsdWRsaG1wc3hoenpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA4MDE0NzgsImV4cCI6MTk5NjM3NzQ3OH0.mgp-oQQJQPkjiZRgDkad40Q5HuY21-le51ziawsVNqQ'
